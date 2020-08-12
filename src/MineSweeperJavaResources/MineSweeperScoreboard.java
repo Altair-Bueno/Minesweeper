108772d73da305814806645d5e317f2d9965c400
Altair-Bueno <altair.bueno@uma.es>
2020-08-12 22:47:10 +0200

    Added Exceptions

--- src/MineSweeperJavaResources/MineSweeperScoreboard.java
+++ src/MineSweeperJavaResources/MineSweeperScoreboard.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Vector;
 
 public class MineSweeperScoreboard {
-    private static Map<String,Integer>map= new HashMap<>();
+    private static final Map<String,Integer>map= new HashMap<>();
 
     public static void addScore(String size,Integer i){
         if (map.containsKey(size)){
